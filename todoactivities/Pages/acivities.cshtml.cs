ba1376f759a42b4803d59325ceadb16eb1f19433
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-29 10:53:03 +0300

    first commit

---
+++ todoactivities/Pages/acivities.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace todoactivities.Pages
+{
+    public class acivitiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
